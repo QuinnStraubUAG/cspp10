7dc41c9e123cd13af7dec8464b455e9e2e56a197
Quinn Straub <quinnstraub@uagateway.org>
2016-10-06 16:04:31 +0000

    Unit 2-1 caultor done

--- unit1/qstraub_simplecalc.py
+++ unit1/qstraub_simplecalc.py
@@ -1,21 +1,46 @@
 number = input(" Type a equation (No spaces plz) :")
+num = int(number[0])
+num_2 = int(number[2])
+
 if number[1] == "+":
-    number = int(number)
-    print(number[0] + number[3])
+    print("The result is " + str(num + num_2))
 elif number[1] == "-":
-    number = int(number)
-    print (number[0] - number[3])
+     print("The result is " + str(num - num_2))
 elif number[1] == "*":
-    number = int(number)
-    print (number[0] * number[3])
-elif number[1] == "/":
-    number = int(number)
-    print (number[0] / number[3])
+    print("The result is " + str(num * num_2))
+elif number[1] == "/": 
+    print("The result is " + str(num / num_2))
 elif number[1] == "%":
-  number = int(number)
-  print (number[0] % number[3])
+    print("The result is " + str(num % num_2))
 else:
-  print ("Didn't input corecty")
+    print ("Didn't input corecty")
+
+
+
+
+
+# if number[1] == "+":
+#     number = int(number[0])
+#     number = int(number[2])
+#     print(number[0] + number[2])
+# elif number[1] == "-":
+#     number = int(number[0])
+#     number = int(number[2])
+#     print (number[0] - number[2])
+# elif number[1] == "*":
+#     number = int(number[0])
+#     number = int(number[2])
+#     print (number[0] * number[2])
+# elif number[1] == "/":
+#     number = int(number[0])
+#     number = int(number[2])
+#     print (number[0] / number[2])
+# elif number[1] == "%":
+#   number = int(number[0])
+#   number = int(number[2])
+#   print (number[0] % number[2])
+# else:
+#  print ("Didn't input corecty")
 # number = input(" Type a equation (No spaces plz) : ")
 # number = int(number)
 # print (number)
