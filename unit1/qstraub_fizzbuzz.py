19f8d4e45ff6ca85b3c4d3c3f634a3edb1d74a10
Quinn Straub <quinnstraub@uagateway.org>
2016-10-27 16:11:07 +0000

    done 3-1

--- unit1/qstraub_fizzbuzz.py
+++ unit1/qstraub_fizzbuzz.py
@@ -1,7 +1,9 @@
-end = input("Put in a number to stop at ")
+start = input("Put a number to start at ")
+end = input("Put a number to end at ")
 end = int(end)
+start = int(start)
 
-for eh in range(1, end):
+for eh in range(start - 1, end):
     eh = eh + 1
     if eh % 3 == 0 and eh % 5 == 0:
         print ("FizzBuzz")
