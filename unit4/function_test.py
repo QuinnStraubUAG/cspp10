c585642ff96887ff32ba538fbe72e2b58d52add2
Quinn Straub <quinnstraub@uagateway.org>
2016-12-21 17:14:51 +0000

    Was testing the Bank for 4-4 its in function test now

--- unit4/function_test.py
+++ unit4/function_test.py
@@ -25,8 +25,27 @@ def bets_cal():
     while ((money != int(money)) or (money > bank_b) or (money < 1)):
         print("It has to be a whole number, not a negtive, and you need to have enogh money")
         money = bets()
-        return ("True")
+    money = int(money)
+    return money
+ 
+def game_thing():
+    return 2
         
         
+def money_system():
+    money_inserted = bets_cal()
+    money_inserted = int(money_inserted)
+    bank_b = begining_bank()
+    bank_b = int(bank_b)
+    bank = bank_b
+    bank = int(bank)
+    win_lose = game_thing()
+    
+    if win_lose == 1:
+        bank = money_inserted * 2 + bank
+        print(bank)
+    elif win_lose == 2:
+        bank = bank - money_inserted
+        print (bank)
         
-bets_cal()
+money_system()
--- unit4/qstraub_craps.py
+++ unit4/qstraub_craps.py
@@ -27,7 +27,8 @@ def bets_cal():
     while ((money != int(money)) or (money > bank_b) or (money < 1)):
         print("It has to be a whole number, not a negtive, and you need to have enogh money")
         money = bets()
-        return money
+    money = int(money)
+    return money
         
         
         
@@ -39,11 +40,12 @@ def game_thing():
     re = 0
     if (roll == 2 or roll == 3 or roll == 12):
         print("You lost")
-        re = 1
+        re = 2
         check = 0 #lose
     elif (roll == 7 or roll == 11):
         print("You Win")
         re = 1
+        #runfunction
         check = 1 #win
     else:
         check = 2 #point
@@ -56,26 +58,43 @@ def game_thing():
         if point_roll == new_roll:
             print("You win your first and Second number matchs")
             re = 1
+            #runfuction
         elif new_roll == 7:
             print ("You lost you rolled 7")
-            re = 1
+            re = 2
         else:
             print("It's a draw let's keep going ontil you ether win or lose")
             input("Press Enter to continue...")
             
-        if re == 1:
+        if re == 1 or re == 2
             y_n = input("Do you want to continue [y|n] (No caps plz): ")
+            #Run bets code
         else:
             fails = fails + 1
             
         if y_n == "y":
             #Run new func
+            print ("I win make win function later")
         elif y_n == "n":
             print("Ok thx for playing")
         else:
             print ("You messed up so now you don't get to go again")
+    return re
             
+
+    
             
+def money_system():
+    money_inserted = bets_cal()
+    bank_b = begining_bank()
+    bank = bank_b
+    win_lose = game_thing()
+    if win_lose == 1:
+        money_inserted * 2 = bank
+    elif win_lose == 2:
+        bank = bank - money_inserted
+        
+        
 
 #  def game_thing_y():
 #     roll = dice_roll()
