2addf5affb52581a2a0454ddd39542f3c439514a
Quinn Straub <quinnstraub@uagateway.org>
2016-12-23 17:09:37 +0000

    I finshed 4-4 craps ... HYPE

--- unit4/function_test.py
+++ unit4/function_test.py
@@ -1,6 +1,5 @@
-def begining_bank():
-    print("You have $100 so... Good luck")
-    return 100 
+# def begining_bank():
+#     return 100 
 
 
 
@@ -8,10 +7,10 @@ def begining_bank():
 
 
 
-def bets ():
-    bets_v = int(input("Put in your bets: "))
-    #This is return the bet
-    return bets_v  
+# def bets ():
+#     bets_v = int(input("Put in your bets: "))
+#     #This is return the bet
+#     return bets_v  
 
 
 
@@ -19,33 +18,40 @@ def bets ():
 
 
 
-def bets_cal():
-    bank_b = begining_bank()
-    money = bets()
-    while ((money != int(money)) or (money > bank_b) or (money < 1)):
-        print("It has to be a whole number, not a negtive, and you need to have enogh money")
-        money = bets()
-    money = int(money)
-    return money
+# def bets_cal():
+#     bank_b = begining_bank()
+#     print("You have $100 so... Good luck")
+#     money = bets()
+#     while ((money != int(money)) or (money > bank_b) or (money < 1)):
+#         print("It has to be a whole number, not a negtive, and you need to have enogh money")
+#         money = bets()
+#     money = int(money)
+#     return money
  
-def game_thing():
-    return 2
+# def game_thing():
+#     return 1
         
         
-def money_system():
-    money_inserted = bets_cal()
-    money_inserted = int(money_inserted)
-    bank_b = begining_bank()
-    bank_b = int(bank_b)
-    bank = bank_b
-    bank = int(bank)
-    win_lose = game_thing()
+# def money_system():
+#     bank_b = begining_bank()
+#     bank_b = int(bank_b)
+#     bank = bank_b
+#     bank = int(bank)
+#     money_inserted = bets_cal()
+#     money_inserted = int(money_inserted)
+#     win_lose = game_thing()
+#     bank = bank - money_inserted
     
-    if win_lose == 1:
-        bank = money_inserted * 2 + bank
-        print(bank)
-    elif win_lose == 2:
-        bank = bank - money_inserted
-        print (bank)
+#     if win_lose == 1:
+#         bank = money_inserted * 2 + bank
+#         print ("Well you won {} dollors so you have {} dollors ".format(money_inserted * 2,bank))
+#         print(bank)
+#     elif win_lose == 2:
+#         print ("Well you lost {} dollors so you have {} dollors left".format(money_inserted,bank))
+#         print (bank)
+#     else:
+#         print("You did it wrong")
         
-money_system()
+        
+# money_system()
+
--- unit4/qstraub_craps.py
+++ unit4/qstraub_craps.py
@@ -1,19 +1,20 @@
 import random
+import sys
 
 def begining_bank():
+    # This is the amount of money you start with
+    # returns money
     return 100 
 def bets ():
     bets_v = int(input("Put in your bets: "))
     #This is return the bet
     return bets_v  
     
-# def bets_Start():
-#     B_bank = begining_bank()
-#     money_gone = bets()
-    
+
 def dice_roll():
     dice1 = random.randint(1,6)
     dice2 = random.randint(1,6)
+    # This randomily rolls two dice and returns there sum
 
     dice_sum = dice1 + dice2
     
@@ -22,6 +23,9 @@ def dice_roll():
 
 def bets_cal():
     bank_b = begining_bank()
+    # Makes sure you do a whole number
+    #returns the money you bet
+    print ("Your starting with $100 so good luck ... ")
     money = bets()
     while ((money != int(money)) or (money > bank_b) or (money < 1)):
         print("It has to be a whole number, not a negtive, and you need to have enogh money")
@@ -33,6 +37,8 @@ def bets_cal():
         
 
 def game_thing():
+    #The main game ...
+    #Returns if you won or lost
     roll = dice_roll()
     fails = 0
     y_n = 0
@@ -64,94 +70,71 @@ def game_thing():
         else:
             print("It's a draw let's keep going ontil you ether win or lose")
             input("Press Enter to continue...")
-            
-        if re == 1 or re == 2:
-            y_n = input("Do you want to continue [y|n] (No caps plz): ")
-            #Run bets code
-        else:
-            fails = fails + 1
-            
-        if y_n == "y":
-            #Run new func
-            print ("I win make win function later")
-        elif y_n == "n":
-            print("Ok thx for playing")
-        else:
-            print ("You messed up so now you don't get to go again")
     return re
             
-
+def contiune():
+    #This ask if you want to play again
+    #It returns if you want to play again or not
+    fails = 0
+    y_n = input("Do you want to continue [y|n] (No caps plz): ")
+    while (y_n != "y") and (y_n != "n"):
+        y_n = input("Do you want to continue [y|n] (No caps plz): ")
+        fails = fails + 1 
+    if y_n == "n":
+        print ("Thanks for playing better luck next time")
+    else: 
+        print("Lets keep going")
+    return y_n
     
+
             
 def money_system():
+    #This find out how much money you will have after a win or lose
+    #returns your final money count
+    fails = 0
     bank_b = begining_bank()
     bank_b = int(bank_b)
     bank = bank_b
     bank = int(bank)
     money_inserted = bets_cal()
-    print ("Your starting with $100 so good luck ... ")
     money_inserted = int(money_inserted)
     win_lose = game_thing()
     bank = bank - money_inserted
     
     if win_lose == 1:
         bank = money_inserted * 2 + bank
-        print(bank)
+        print ("Well you won {} dollors so you have {} dollors ".format(money_inserted * 2,bank))
     elif win_lose == 2:
-        bank = bank - money_inserted
-        print (bank)
+        print ("Well you lost {} dollors so you have {} dollors left".format(money_inserted,bank))
     else:
-        print("You did it wrong")
-        
+        fails = fails + 1
+    return (bank)
 
-#  def game_thing_y():
-#     roll = dice_roll()
-#     fails = 0
-#     y_n = 0
-#     re = 0
-#     if (roll == 2 or roll == 3 or roll == 12):
-#         print("You lost")
-#         re = 1
-#         check = 0 #lose
-#     elif (roll == 7 or roll == 11):
-#         print("You Win")
-#         re = 1
-#         check = 1 #win
-#     else:
-#         check = 2 #point
-#         print("You didn't land the things ... POINT ROUND")
-#     new_roll = 0
-#     point_roll = roll
-#     while (check == 2) and (point_roll != new_roll and new_roll != 7):
-#         new_roll = dice_roll()
-#         print ("Your roll was {} it rolled {} ".format(point_roll,new_roll))
-#         if point_roll == new_roll:
-#             print("You win your first and Second number matchs")
-#             re = 1
-#         elif new_roll == 7:
-#             print ("You lost you rolled 7")
-#             re = 1
-#         else:
-#             print("It's a draw let's keep going ontil you ether win or lose")
-#             input("Press Enter to continue...")
-            
-#         if re == 1:
-#             y_n = input("Do you want to continue [y|n] (No caps plz): ")
-#         else:
-#             fails = fails + 1
-            
-#         if y_n == "y":
-#             #Run new func
-#         elif y_n == "n":
-#             print("Ok thx for playing")
-#         else:
-#             print ("You messed up so now you don't get to go again")
-            
 
 def craps():
-    # bet_test = bets_cal()
-    game = money_system()
-   
+    #This runs the game and runs a new money systerm witch is pretty much the same as the last but your old money will transfer
+    fails = 0
+    bank = money_system()
+    bank = int(bank)
+    contin = contiune()
+    while contin == "y":
+        if bank == 0:
+            sys.exit()
+        money_inserted = bets()
+        money_inserted = int(money_inserted)
+        while ((money_inserted > bank) or (money_inserted < 1)):
+            print("It has to be a whole number, not a negtive, and you need to have enogh money")
+            money_inserted = bets()
+        win_lose = game_thing()
+        bank = bank - money_inserted
+        if win_lose == 1:
+            bank = money_inserted * 2 + bank
+            print ("Well you won {} dollors so you have {} dollors ".format(money_inserted * 2,bank))
+        elif win_lose == 2:
+            print ("Well you lost {} dollors so you have {} dollors left".format(money_inserted,bank))
+        else:
+            fails = fails + 1
+        contin = contiune()
 craps()
 
 #work in progess
